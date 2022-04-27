6a116429b26e920e0772f730f2413a914b5b0342
Jessica <jessica1329b@gmail.com>
2022-04-26 22:23:05 -0400

    Javascript Operators

--- app.js
+++ app.js
@@ -6,14 +6,51 @@ console.log(
 // Exercise 1
 console.log("EXERCISE 1:\n==========\n");
 
-// YOUR CODE HERE
+let a = 20;
+let b = 4;
+
+let add = a + b;
+let minus = minus * a;
+let divide = multiply / b;
+let mod = divide % b;
+
+let complicatedMath = a * b - a / (b + a);
+
+console.log(mod);
 
 // Exercise 2
 console.log("EXERCISE 2:\n==========\n");
 
-// YOUR CODE HERE
+let a  = 11;
+let str = "11";
+let str2 = "eleven";
+let isPresent = true;
+let firstName = "Jackie";
+let lastName = "Chan";
+
+a + str;
+a + str2;
+a + isPresent;
+
+
+console.log(a + isPresent);
+console.log(a + firstName);
+console.log(a + lastName);
 
 // Exercise 3
 console.log("EXERCISE 3:\n==========\n");
 
-// YOUR CODE HERE
+console.log(d == str3);
+console.log(d ===str3);
+console.log(!isPresent2);
+console.log("eleven" == str4 && d >= str3);
+console.log(!isPresent2 || isPresent2)?
+console.log(0 == false);
+console.log(0 === false);
+console.log(0 != false);
+console.log(0 !== false);
+
+console.log(undefined == null);
+
+
+
